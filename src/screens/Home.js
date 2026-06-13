44cdb06a3c8d54e10bb1b7c4c94586a5c3da3749
bautistagorchs <bautista.gorchs@gmail.com>
2026-06-13 13:06:25 -0300

    feat: crear mi perfil y home boilerplate

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,20 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { auth } from "../config/firebase";
 
-function Home(params) {
-  return <Text>Home</Text>;
-}
+const Home = ({ navigation }) => {
+  console.log(auth.currentUser);
+  return <Text>Bienvenido a Cortado</Text>;
+};
+
+const s = StyleSheet.create({
+  button: {
+    backgroundColor: "#4f46e5",
+    width: "100%",
+    paddingVertical: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 8,
+  },
+});
 
 export default Home;
--- src/screens/MiPerfil.js
+++ src/screens/MiPerfil.js
@@ -1,7 +1,34 @@
-import { Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { auth } from "../config/firebase";
 
-function MiPerfil(params) {
-  return <Text>Mi perfil</Text>;
-}
+const MiPerfil = ({ navigation }) => {
+  const logout = () => {
+    auth.signOut();
+    // .then(() => navigation.navigate("Login"))
+    // .catch((error) => {
+    //   console.error("Logout error:", error);
+    //   alert("Logout failed: " + error.message);
+    // });
+    navigation.navigate("Login");
+  };
+  return (
+    <View>
+      <Pressable style={s.button} onPress={logout}>
+        <Text>Logout</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const s = StyleSheet.create({
+  button: {
+    backgroundColor: "#4f46e5",
+    width: "100%",
+    paddingVertical: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 8,
+  },
+});
 
 export default MiPerfil;
